2557db15458afd1efdaba95f46782edc9153325d
Jules Maselbas <jules.maselbas@grenoble-inp.org>
2017-04-08 19:43:38 +0200

    Main refactor

--- src/compileBST.c
+++ src/compileBST.c
@@ -17,14 +17,22 @@
 
 
 
-/**
- * Main function
- * \brief Main function
- * \param argc  A count of the number of command-line arguments
- * \param argv  An argument vector of the command-line arguments.
- * \warning Must be called with a positive long integer, n,  and a filename, freqFile, as commandline parameters and in the given order.
- * \returns { 0 if succeeds and prints C code implementing an optimal ABR on stdout; exit code otherwise}
- */
+void print_usage(FILE* f) {
+    fprintf(f, "Usage: compileBST <n> <file>\n");
+}
+
+void print_strtol_error(FILE* f, const int err, const char *str) {
+    switch (err) {
+    case EINVAL :
+        fprintf(f, "%s does not match a long integer value. \n", str);
+        break;
+    case ERANGE :
+        fprintf(f, "%s does not fit in a long int :\n" "out of bound [%ld;%ld]\n", str, LONG_MIN, LONG_MAX);
+        break;
+    default:
+        break;
+    }
+}
 
 void parser(FILE* freqFile, int n, int* tab){
   int c;
@@ -70,67 +78,47 @@ void optABR(int* valeurs, int n, int m, int** poidsABR, int* poids, int N){
 }
 
 
+
+/**
+ * Main function
+ * \brief Main function
+ * \param argc  A count of the number of command-line arguments
+ * \param argv  An argument vector of the command-line arguments.
+ * \warning Must be called with a positive long integer, n,  and a filename, freqFile, as commandline parameters and in the given order.
+ * \returns { 0 if succeeds and prints C code implementing an optimal ABR on stdout; exit code otherwise}
+ */
 int main (int argc, char *argv[]) {
   long n = 0 ; // Number of elements in the dictionary
-  FILE *freqFile = NULL ; // File that contains n positive integers defining the relative frequence of dictinary elements
+  FILE *freqFile = NULL ; // File that contains n positive integers defining the relative frequence of dictionary elements
 
   if(argc != 3){
-    fprintf(stderr, "!!!!! Usage: ./compileBST n  originalFile !!!!!\n");
+      print_usage(stderr);
       exit(EXIT_FAILURE); /* indicate failure.*/
   }
 
-  { // Conversion of parameter n in a long
-    int codeRetour = EXIT_SUCCESS;
-    char *posError;
-    long resuLong;
-    n = atol(argv[1] ) ;
 
-    assert(argc >= 2);
-    // Conversion of argv[1] en long
-    resuLong = strtol(argv[1], &posError, 10);
-    // Traitement des erreurs
-    switch (errno)
-    {
-      case EXIT_SUCCESS :
-         // Conversion du long en int
-         if (resuLong > 0)
-         {
-            n = (long)resuLong;
-            fprintf(stderr, "Number of elements in the dictionary: %ld\n", n);
-         }
-         else
-         {
-            (void)fprintf(stderr, "%s cannot be converted into a positive integer matching the number of elements in the dicionary.\n", argv[1]) ;
-            codeRetour = EXIT_FAILURE;
-         }
-      break;
-
-      case EINVAL :
-         perror(__func__);
-         (void)fprintf(stderr, "%s does not match a long integer value. \n", argv[1]);
-         codeRetour = EXIT_FAILURE;
-      break;
-
-      case ERANGE :
-         perror(__func__);
-         (void)fprintf(stderr, "%s does not fit in a long int :\n" "out of bound [%ld;%ld]\n",
-                       argv[1], LONG_MIN, LONG_MAX);
-         codeRetour = EXIT_FAILURE;
-      break;
-      default :
-         perror(__func__);
-         codeRetour = EXIT_FAILURE;
-    } // switch (errno)
-    if  (codeRetour != EXIT_SUCCESS) {
-      return codeRetour ;
-    }
-
+  // Conversion of argv[1] en long
+  n = strtol(argv[1], NULL, 10);
+  // Traitement des erreurs
+  if (errno != EXIT_SUCCESS) {
+      print_strtol_error(stderr, errno, argv[1]);
+      perror(__func__);
+      exit(EXIT_FAILURE);
   }
+  if (n < 0) {
+      fprintf(stderr, "%s cannot be converted into a positive integer.\n", argv[1]);
+      exit(EXIT_FAILURE);
+  } else if (n < 3) {
+      fprintf(stderr, "The number of elements in the dictionary must be greater than 2.\n");
+      exit(EXIT_FAILURE);
+  }
+  fprintf(stderr, "Number of elements in the dictionary: %ld\n", n);
 
+  // Opening dictionary file
   freqFile = fopen(argv[2] , "r" );
   if (freqFile == NULL) {
-    fprintf (stderr, "!!!!! Error opening originalFile !!!!!\n");
-    exit(EXIT_FAILURE);
+      fprintf(stderr, "FAIL to open file '%s' !\n", argv[2]);
+      exit(EXIT_FAILURE);
   }
 
   int valeurs[n]; //Stockage des valeurs lues dans le fichier
